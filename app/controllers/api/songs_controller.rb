cd79f9e72ea26c72dbbfaea7f79deb04a18c6918
Ambar <ambarmogg@me.com>
2020-11-10 17:25:03 -0800

    restful index action and restful show action

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,2 +1,12 @@
 class Api::SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+    render 'index.json.jb'
+  end
+
+  def show
+    @song = Song.find_by(id:1)
+    render 'show.json.jb'
+  end
 end

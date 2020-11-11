41c1b1bf9dde17247c4f69c93af71e2338f2ef8d
Ambar <ambarmogg@me.com>
2020-11-10 17:34:31 -0800

    restful create action

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -9,4 +9,16 @@ class Api::SongsController < ApplicationController
     @song = Song.find_by(id:1)
     render 'show.json.jb'
   end
+
+  def create
+    @song = Song.new(
+      title: params[:title],
+      album: params[:album],
+      artist: params[:artist],
+      year: params[:year]
+    )
+    @song.save
+    render 'show.json.jb'
+  end
 end
+
